f09d256eb5b455c941a3eff9015cd65f6f1e4cfa
Fikasentani <mpilo.dlamini@yahoo.com>
2019-10-09 06:17:00 +0200

    added objects to modularize queryselector similar to dictionaries implemeted on test automation beeter implementation and understandig of IIFEs

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
 //BUDGET CONTROLLER
-var budgetController = (function(){
-    
+var budgetController = (function () {
+
 })();
 
 //UI CONTROLLER
-var UIController = (function() {
+var UIController = (function () {
 
+    var DOMstrings = {
+        addType: '.add__type',
+        addDescription: '.add__description',
+        addValue: '.add__value',
+        addBtn: '.add__btn',
+
+    }
+    return {
+        getInput: function () {
+            return {
+                 type: document.querySelector(DOMstrings.addType).value, //will be either inc or exp
+                 description: document.querySelector(DOMstrings.addDescription).value,
+                 value: document.querySelector(DOMstrings.addValue).value
+            };
+
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 })();
 
 //GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
 
-    var ctrlAddItem = function() {
+    var setupEventListeners = function() {
+        var getCtrlDOM = UICtrl.getDOMstrings();
+        document.querySelector(getCtrlDOM.addBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        // console.log(event.key);
+        if (event.key == "Enter") {
+            ctrlAddItem();
+        }
+
+    });
+    };
+    
+    var ctrlAddItem = function () {
         //1. Get field iput data
-
+        var input = UICtrl.getInput();
+        console.log(input);
         //2. Add the item to the budget controller
 
         //3. Add the item to the UI
@@ -22,16 +57,17 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         //5. Display the budget on the UI
 
-        console.log("mub");
-    }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        // console.log(event.key);
-        if (event.key == "Enter") {
-            ctrlAddItem();
-        }
         
-    });
+    };
+
+    return {
+        init: function() {
+            console.log("mpi");
+            setupEventListeners();
+        }
+    };
+    
 
 })(budgetController, UIController);
+
+controller.init();
